aad1d0023623057cc389ff3f4997bf5696023655
zzamai <vinicius.zamai@outlook.com>
2021-11-17 17:25:43 -0300

    carrossel v1

--- AngularPoke/src/app/catcher/catcher.component.ts
+++ AngularPoke/src/app/catcher/catcher.component.ts
@@ -1,17 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-catcher',
   templateUrl: './catcher.component.html',
-  styleUrls: ['./catcher.component.scss']
+  styleUrls: ['./catcher.component.scss'],
+  template: `
+    <section class="wrapper" id="wrapper">
+  `
 })
 export class CatcherComponent implements OnInit {
 
   pokeInfo?: String;
 
-  constructor() { }
+  constructor(private wrapper:ElementRef) {
+    
+  }
 
   ngOnInit(): void {
+    let container = this.wrapper?.nativeElement.querySelector('#wrapper')
+    container.addEventListener('wheel', (e: WheelEvent) => {
+      if (e.deltaY > 0) this.scroll(1)
+      else this.scroll(0)
+    });
+  }
+
+  scroll(flag: Number) {
+    if (flag == 1) this.wrapper.nativeElement.querySelector('#wrapper').scrollLeft += 100
+    else this.wrapper.nativeElement.querySelector('#wrapper').scrollLeft -= 100
   }
 
 }
